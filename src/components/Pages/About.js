1ff0aaf41fc066053c0cbd7e344652d05ab7aa2e
ism22dana <dana_ismagulova2003@mail.ru>
2022-04-25 13:14:12 +0600

    render

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,11 +1,90 @@
 import classes from './NavPages.module.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+// Message Component
+function Message(props)
+{
+    if (props.isLoggedIn)
+        return <h1>Welcome User</h1>;
+    else
+        return <h1>Please Login</h1>;
+}
+ 
+// Login Component
+function Login(props)
+{
+   return(
+           <button onClick = {props.clickFunc}>
+               Login
+           </button>
+       );
+}
+ 
+// Logout Component
+function Logout(props)
+{
+    return(
+           <button onClick = {props.clickFunc}>
+               Logout
+           </button>
+       );
+}
+ 
+// Parent Homepage Component
+class Homepage extends React.Component{
+ 
+    constructor(props)
+    {
+        super(props);
+ 
+        this.state = {isLoggedIn : false};
+ 
+        this.ifLoginClicked = this.ifLoginClicked.bind(this);
+        this.ifLogoutClicked = this.ifLogoutClicked.bind(this);
+    }
+ 
+    ifLoginClicked()
+    {
+        this.setState({isLoggedIn : true});
+    }
+ 
+    ifLogoutClicked()
+    {
+        this.setState({isLoggedIn : false});
+    }
+ 
+    render(){
+ 
+        return(
+ 
+            <div>
+ 
+                <Message isLoggedIn = {this.state.isLoggedIn}/>
+                 
+                {
+                    (this.state.isLoggedIn)?(
+                    <Logout clickFunc = {this.ifLogoutClicked} />
+                    ) : (
+                    <Login clickFunc = {this.ifLoginClicked} />
+                    )
+                }
+ 
+            </div>
+                 
+            );
+    }
+}
+ 
+ReactDOM.render(
+    <Homepage />,
+    document.getElementById('root')
+);
 
 const About = () =>{
     return (
         <div className={classes.about}>
-            <div className={classes.container}>
-
-            </div>
+            <div className={classes.container}>      <Homepage />   </div>
         </div>
     );
 }
