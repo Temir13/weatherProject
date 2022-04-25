dfae92656a4ae86fbe2fd23468aa08acf7097f95
Temir <31662@iitu.edu.kz>
2022-04-25 11:46:36 +0600

    fixed state

--- src/components/Pages/Page2.js
+++ src/components/Pages/Page2.js
@@ -3,15 +3,24 @@ import { Component } from 'react';
 import classes from './NavPages.module.css'
 export default class Page2 extends Component{
     state = {
-        title:'USSR',
-        content:'FALL'
+    
     }
 
     render(){
+        const name = 'Юность'
+        const content = 'Звук поставим на всю и соседи не спят'
+        const content2 = 'Кто под нами внизу, вы простите меня'
+        const content3 = 'А потом о любви говорить до утра'
+        const content4 = 'Это юность моя, это юность моя'
+        
         return (
             <div className={classes.Page2}>
                 <div className={classes.container}>
-                {this.state.title}
+                {<h2>{name}</h2>}
+                {<h3>{content}</h3>}
+                {<h3>{content2}</h3>}
+                {<h3>{content3}</h3>}
+                {<h3>{content4}</h3>}
                 </div>        
             </div>
         );
