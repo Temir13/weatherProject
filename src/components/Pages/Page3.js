e498f6751c7a4b9d3da162dc347c907d2dcf7d56
Temir <31662@iitu.edu.kz>
2022-05-06 09:48:42 +0600

    language fix in blogs

--- src/components/Pages/Page3.js
+++ src/components/Pages/Page3.js
@@ -6,27 +6,27 @@ const Page3 = () =>{
     return (
         <div className={classes.Page3}>
             <div className={classes.container}>
-                <h2>Пользовательские блоги</h2>
+                <h2>User blogs</h2>
                 <div className={classes.content}>
                 <Proptest 
                    BlogPhoto={'https://i.insider.com/593af232bf76bb72038b4c7a?width=1000&format=jpeg&auto=webp'}     
-                   BlogDescryption='Мир на гране ядерной войны, или нет? что же нам делать в этот не простой период? Ответ прост. Нам понадобятся веревка и мыло....'  
+                   BlogDescryption='Is the world on the brink of nuclear war, or not? what should we do in this difficult period? The answer is simple. Well need rope and soap....'  
                     />
                     <Proptest 
                    BlogPhoto={'https://i.pinimg.com/736x/51/1f/b7/511fb758c7caafc880e44e86c3a8515a--wallpapers-android-anime-art.jpg'}     
-                   BlogDescryption='Почему Харухи самое лучшее аниме на  всем всем? Потому что. '  
+                   BlogDescryption='Why is Haruhi the best anime on everything? Because.'  
                     />
                     <Proptest 
                    BlogPhoto={'https://cdn.prod.www.spiegel.de/images/f7bc7900-0001-0004-0000-000000300229_w1528_r1.4512847965738758_fpx49.58_fpy50.jpg'}     
-                   BlogDescryption='Что делать, если после землятресения у Вас пропал весь смысл жизни? Дом, машина, скидочная карта в Fun Day, все это теперь лишь груда мусора? Не беда, Вам всего лишь нужна веревка и мыло...'  
+                   BlogDescryption='What to do if after an earthquake you lost the whole meaning of life? A house, a car, a discount card in FunDay, all this is now just a pile of garbage? It doesnt matter, you just need a rope and soap..'  
                     />
                     <Proptest 
                    BlogPhoto={'https://avatars.mds.yandex.net/get-zen_doc/1585599/pub_5e2f27540ce57b00ae333640_5e2f281ac31e4900b03bee74/scale_1200'}     
-                   BlogDescryption='Глобальное потепление. Причины формирование и анализ. '  
+                   BlogDescryption='Global warming. Causes formation and analysis.'  
                     />
                     <Proptest 
                    BlogPhoto={'https://gtrkkursk.ru/sites/default/files/news/4102/preview-1234802024.jpg'}     
-                   BlogDescryption='Продам Гараж'  
+                   BlogDescryption='I will sell the Garage'  
                     />
 
                 </div>
