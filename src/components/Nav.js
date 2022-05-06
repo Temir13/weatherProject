b927383c57b4365768440676191c756f76847b5e
ism22dana <dana_ismagulova2003@mail.ru>
2022-05-06 08:22:29 +0600

    Revert "news"

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,8 +6,8 @@ const nav = () =>{
         <nav className={classes.nav}>
             <div className={classes.container}>
                 <div className={classes.menuContainer}>
-                    <a href='/Home' className={classes.menu}>Home</a>   
-                    <Link to='/Page2' className={classes.menu}>States</Link>  
+                    <a href='/Home' className={classes.menu}>Weather</a>   
+                    <Link to='/Page2' className={classes.menu}>News</Link>  
                     <Link to='/Page3' className={classes.menu}>Props</Link>  
                     <Link to='/About' className={classes.menu}>About</Link>               
                 </div>
