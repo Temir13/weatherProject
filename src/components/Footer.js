9aa60cb05e055ffe8e0d4c0a59017ca1a7dc2967
Kesha1616 <102520329+Kesha1616@users.noreply.github.com>
2022-05-06 07:44:35 +0600

    footer finish

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,36 @@ class Footer extends Component{
     render(){
     return (
         <footer className={classes.footer}>
-            footer
+            <div className="container" >
+    <div class="row">
+      
+      <div className="col-lg-4 col-md-6">
+        <h3>Everything that kills me, makes me feel alive</h3>
+
+      </div>
+      
+      <div class="col-lg-4col-md-6">
+        <h3>ABOUT</h3>
+        <div class="media">
+
+          <div class="media-body">
+            <h4 class="media-heading">+7 (707) 707 07 07</h4>
+            <p>weather@gmail.com</p>
+          </div>
+        </div>
+        
+      </div>
+      
+      <div className='last'>
+        <h3>Thanks for YOU</h3>
+      </div>
+      
+    </div>
+  </div>
+  <div class="copyright text-center">
+    Copyright &copy; 2022 <span>TDE</span>
+    </div>
+
         </footer>
 
     );
